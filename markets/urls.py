fca236e26da805ce607fd2f3debfa22d8692a3cb
ISTC\ITSC <ys_lee0716@naver.com>
2025-02-24 18:30:31 +0900

    25. 02. 24 1. 추가사항   - markets  앱관련      1) 모델 생성         ① Market         ② HashTag         ③ Festival_id         ④ MarketImage
          2) templates\markets 폴더 및 템플릿(초안) 생성
            ① market_add.html
            ② market_detail.html
            ③ market_list.html
            ④ tags.html
    
          3) views 생성
            ① market_add
            ② market_detail
            ③ market_list
    
          4) urls path 추가
            ① market_add
            ② market_detail

--- markets/urls.py
+++ markets/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from markets import views
 
+app_name = "markets"
 urlpatterns = [
-    # path("", views.),
-    
+    path("market_add/", views.market_add, name = "market_add"),
+    path("<int:market_id>/", views.market_detail, name = "market_detail"),
 ]
