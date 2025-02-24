fdff04d82be126bd16935d3c23641bb9a615eeb1
Shin Buya <buya1102@naver.com>
2025-02-24 12:58:35 +0900

    1. 추가사항    1) createsuperuser로 관리자 계정 생성       id: sdp_admin       pw: 7777    2) users/models.py 에 AbstractUser을 상속받아 CustomUser 모델생성    3) users/admin.py에 모델 등록 후 fieldsets 정의    4) sdp/settings.py 에 CustomUser모델 정의

--- SDP/settings.py
+++ SDP/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 
 # 커스텀 User 모델 사용
-# AUTH_USER_MODEL = "users.User"
+AUTH_USER_MODEL = "users.User"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
--- users/models.py
+++ users/models.py
@@ -1,7 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
+class User(AbstractUser):
+    username = models.CharField(max_length=32, primary_key = True, verbose_name='아이디')
+    password = models.CharField(max_length=128, verbose_name = '비밀번호')
+    email = models.CharField(max_length=40, verbose_name = '이메일')
+    business_number = models.CharField(max_length=10, blank = True, verbose_name = '사업자번호')
+    profile_image = models.ImageField(upload_to="users/profile", blank = True, verbose_name='프로필사진')
 
 # class User(models.Model):
 #    pass
