b45016d15dda994caad9f7a6f52f570a0259cc78
lee namgyung <8o8__10@naver.com>
2025-02-28 11:41:29 +0900

    외부접속 가능하도록 setting 변경

--- SDP/settings.py
+++ SDP/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-u1u2!sw=7l$231rv=sd9f-6w(ukhrcl&xg26d6ouw@xydf-lnx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["192.168.3.5"]
 
 
 # Application definition
